3ad7609255eac37d1e5c4bcc84cef7a80eb9319a
colliona <bmdenis95@gmail.com>
2018-12-08 15:16:38 +0100

    yolocaust

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/anime');
 let db = mongoose.connection;
 
